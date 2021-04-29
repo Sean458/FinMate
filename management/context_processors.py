d31c34c4da338aa1738400b6b3d5a23b1b6e9eba
MerwinSamuel <merwinsamuel7@gmail.com>
2021-04-29 16:13:44 +0530

    fixed summary report query

--- management/context_processors.py
+++ management/context_processors.py
@@ -3,6 +3,6 @@ from .models import Notification
 
 def top_notifications(request):
     if request.user.is_authenticated:
-        return {'top_notifications': Notification.objects.filter(user=request.user)}
+        return {'top_notifications': Notification.objects.filter(user=request.user).order_by('-created_date')}
     else:
         return {'top_notifications': Notification.objects.all()}
